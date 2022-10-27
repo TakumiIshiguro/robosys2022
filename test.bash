b4c85e93010eb7bc0875b0a672a5095da3f55d64
Takumi Ishiguro <s21c1011rx@s.chibakoudai.jp>
2022-10-27 12:29:47 +0900

    test

--- test.bash
+++ test.bash
@@ -1,3 +1,18 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Takumi Ishiguro
+# SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 14 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo ok
+exit $res
+
+
