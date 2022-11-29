bd7a87ceff8acdb80bd0286bd458934e19de0e11
Takumi Ishiguro <s21c1011rx@s.chibakoudai.jp>
2022-11-29 16:07:45 +0900

    test

--- test.bash
+++ test.bash
@@ -3,24 +3,23 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo NG at Line $1
-	res=1
+        echo NG at Line $1
+       	res=1
 }
 
 res=0
++ res=0
 
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
-[ "$[out]" = "" ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
-out=$(echo  | ./plus)
+out=$(echo | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
-[ "$[out]" = "" ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
-[ "$res" = 0 ] && echo ok
+[ "$res" = 0 ] && echo OK
 exit $res
-
-
